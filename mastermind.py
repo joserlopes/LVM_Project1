de848b3ab46d02b3913c40064073d85f18d3f4e0
José Lopes <josersl2003@gmail.com>
2025-10-07 13:18:16 +0100

    Full mastermind is now working properly.

--- mastermind.py
+++ mastermind.py
@@ -14,8 +14,6 @@ def mastermind2(B):
     P = [[Bool(f"P_{i}_{n}") for n in range(10)] for i in range(1, guess_len + 1)]
     s = Solver()
 
-    # Since we only have black pegs, consider the number of combinations of size `peg_value`
-    # because we know for sure one of those combinations is right
     for guess_i, black_peg_i, white_peg_i in zip(
         range(0, len(B), 3), range(1, len(B), 3), range(2, len(B), 3)
     ):
@@ -24,50 +22,38 @@ def mastermind2(B):
         white_peg = B[white_peg_i]
 
         guess_with_indices = list(enumerate(guess))
-        if black_peg == 0 and white_peg == 0:
+        if black_peg == 0:
             for value_i, value in guess_with_indices:
                 s.add(Not(P[value_i][value]))
-        else:
-            black_combinations_list = combinations(guess_with_indices, black_peg)
-            # Essentially what this is doing is it adds a clause for each combination that says
-            # that the numbers in combination can be part of the right solution and the others
-            # not so...
-            clauses_to_add = []
-            for black_combination in black_combinations_list:
-                available_spots = [
-                    x[0] for x in guess_with_indices if x not in black_combination
-                ]
-                for white_combination in combinations(available_spots, white_peg):
-                    for white_position in white_combination:
-                        clauses_to_add.append(
-                            Not(P[white_position][guess[white_position]])
-                        )
-                    for white_permutation in permutations(available_spots, white_peg):
-                        if any(
-                            white_permutation[i] == white_combination[i]
-                            for i in range(white_peg)
-                        ):
-                            continue
+        black_combinations_list = combinations(range(guess_len), black_peg)
+        clauses_to_add = []
+        for black_combination in black_combinations_list:
+            locked_present = []
+            for black_i in black_combination:
+                locked_present.append(P[black_i][guess[black_i]])
+            available_spots = [
+                x for x in range(guess_len) if x not in black_combination
+            ]
+            for white_combination in combinations(available_spots, white_peg):
+                not_present = []
 
-                        present = []
-                        not_present = []
-                        white_available_spots = [
-                            x for x in available_spots if x not in white_permutation
-                        ]
-                        print("White available spots:", white_available_spots)
-                        for original_position, new_position in zip(
-                            white_combination, white_permutation
-                        ):
-                            print("Original position:", original_position)
-                            print("New position:", new_position)
-                            present.append(P[new_position][guess[original_position]])
-                            for white_available_spot in white_available_spots:
-                                not_present.append(
-                                    Not(P[white_available_spot][guess[new_position]])
-                                )
-                        clauses_to_add.append(And(present + not_present))
-                    s.add(Or(clauses_to_add))
-                    print(s)
+                for white_position in white_combination:
+                    not_present.append(Not(P[white_position][guess[white_position]]))
+
+                for white_permutation in permutations(available_spots, white_peg):
+                    present = []
+                    if any(
+                        white_permutation[i] == white_combination[i]
+                        for i in range(white_peg)
+                    ):
+                        continue
+
+                    for original_position, new_position in zip(
+                        white_combination, white_permutation
+                    ):
+                        present.append(P[new_position][guess[original_position]])
+                    clauses_to_add.append(And(locked_present + present + not_present))
+        s.add(Or(clauses_to_add))
 
     # Each position needs to have a number
     for pos in P:
@@ -119,5 +105,5 @@ B2 = [
     1,
     2,
 ]
-# print(mastermind2(B1))
-print(mastermind2(B2))
+print(mastermind2(B1))
+# print(mastermind2(B2))
