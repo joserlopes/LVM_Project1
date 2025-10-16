98b5507e74a9e11ba307a753f6262e70f8d089fc
zeserrado-marques <zeserradommarques@gmail.com>
2025-10-16 22:12:27 +0100

    em principio, estão todos os gráficos feitos

--- generate_boards.sh
+++ generate_boards.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-len=8
+f=20
 
-for f in `seq 10 2 50`
+for len in `seq 4 1 9`
 do
-    outfile=boards/"boards_increasing_guesses_len_$len"
+    outfile=boards/"boards_increasing_len_guess_$f"
     echo "Create board of with len of $len and $f guesses"
     python3 chat_board_generator.py "$f" "$len" >> "$outfile"
     echo "generation done len = $len, guess num = $f"
